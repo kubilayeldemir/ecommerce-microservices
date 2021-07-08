97e977455a4abb2102338025ed8da81dbc76cd04
kubilayeldemir <kubi0707@gmail.com>
2021-07-08 15:27:46 +0300

    feat: Order Service Swagger Impl.

--- OrderService/src/main/java/com/Microservice/OrderService/Controller/OrderController.java
+++ OrderService/src/main/java/com/Microservice/OrderService/Controller/OrderController.java
@@ -2,11 +2,13 @@ package com.Microservice.OrderService.Controller;
 
 import com.Microservice.OrderService.Model.Order;
 import com.Microservice.OrderService.Repository.OrderRepository;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@Api(value = "Order Api")
 public class OrderController {
     OrderRepository orderRepository;
 
--- OrderService/src/main/java/com/Microservice/OrderService/OrderServiceApplication.java
+++ OrderService/src/main/java/com/Microservice/OrderService/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.Microservice.OrderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
