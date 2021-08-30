285ba1dcdf2891645b5166214b291a0d0a48aaf5
Kubilay Eldemir <kubi0707@gmail.com>
2021-08-30 16:09:33 +0300

    Docker: docker-compose redis for basket service

--- basket-microservice/redis.js
+++ basket-microservice/redis.js
@@ -1,10 +1,8 @@
 const redis = require("redis");
 
-const client = redis.createClient(process.env.REDIS_URL, {
-    tls: {
-        rejectUnauthorized: false
-    }
-});
+const client = redis.createClient({
+    host: process.env.REDIS_URL,
+})
 
 client.on('connect', () => {
     console.log("client connected")
