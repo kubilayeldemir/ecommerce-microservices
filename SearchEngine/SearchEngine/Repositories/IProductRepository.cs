d048d8ede100a5f3cd336dca79b16e8beab4498e
kubilayeldemir <kubi0707@gmail.com>
2021-12-14 22:49:26 +0300

    Search(feat): Query products with ES combined fields

--- SearchEngine/SearchEngine/Repositories/IProductRepository.cs
+++ SearchEngine/SearchEngine/Repositories/IProductRepository.cs
@@ -8,6 +8,7 @@ namespace SearchEngine.Repositories
 {
     public interface IProductRepository
     {
+        public Task<List<Product>> QueryCombineFields(string query, int from, int size);
         public Task<List<Product>> Get(GetProductRequestModel model, int from, int size = 10);
         public  Task<Guid> Save(Product product);
         public Task<List<Guid>> BulkSave(List<Product> productList);
